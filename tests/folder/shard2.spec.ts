22af21094705efd571bfbfbda5bab6826b53fa9c
Max Richter <max.richter@understand.ai>
2024-10-02 12:03:20 +0200

    add more tests

--- tests/folder/shard2.spec.ts
+++ tests/folder/shard2.spec.ts
@@ -7,4 +7,17 @@ test.describe('shard', () => {
         // Expect a title "to contain" a substring.
         await expect(page).toHaveTitle(/Playwright/);
     });
+
+    test('get started link', async ({page}) => {
+        await page.goto('https://playwright.dev/');
+        // Click the get started link.
+        await page.getByRole('link', {name: 'Get started'}).click();
+        // Expects page to have a heading with the name of Installation.
+        await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible();
+    });
+
+    test('search button', async ({page}) => {
+        await page.goto('https://playwright.dev/');
+        await expect(page.getByRole('button', {name: 'Search'})).toBeEnabled();
+    })
 })
