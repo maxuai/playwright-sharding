6411eb4f7e003951fa0add50968b460639190596
Max Richter <max.richter@understand.ai>
2024-10-04 08:03:18 +0200

    - add `fullyParallel`

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import {defineConfig, devices} from '@playwright/test';
 export default defineConfig({
     testDir: './tests',
     retries: 0,
+    fullyParallel: true,
     workers: process.env.SHARDING ? 1 : undefined,
     reporter: process.env.SHARDING ? [['blob'], ['list']] : 'list',
     use: {
