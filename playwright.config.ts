310abd13ee345fc7c9c320d7cfbf1a25d85ecae8
Max Richter <max.richter@understand.ai>
2024-10-07 09:00:46 +0200

    - disable fully parallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import {defineConfig, devices} from '@playwright/test';
 export default defineConfig({
     testDir: './tests',
     retries: 0,
-    fullyParallel: true,
+    fullyParallel: false,
     workers: process.env.SHARDING ? 1 : undefined,
     reporter: process.env.SHARDING ? [['blob'], ['list']] : 'list',
     use: {
